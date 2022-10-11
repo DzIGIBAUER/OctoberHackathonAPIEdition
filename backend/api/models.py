fbf6a19cd469e8c799a297c6e85b04bea64b5f80
DzIGIBAUER_PC <milosh2255@gmail.com>
2022-10-11 19:39:42 +0200

    Added djangorestframework-jsonapi and Pillow. Added Advocate, Company and Link models.

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Advocate(models.Model):
+    name = models.CharField(max_length=50)
+    profile_image = models.ImageField(upload_to='profile_images')
+    short_bio = models.CharField(max_length=150)
+    long_bio = models.TextField()
+    advocate_years_exp = models.PositiveSmallIntegerField(default=0)
+    company = models.ForeignKey('Company', models.SET_DEFAULT, related_name='advocates', default=None)
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    logo = models.ImageField(upload_to='company_logos')
+    summary = models.TextField()
+
+
+class Link(models.Model):
+    advocate = models.OneToOneField(Advocate, models.CASCADE)
+    _fields = ['youtube', 'github', 'twitter'] # run migration after change!
+
+
+for field_name in Link._fields:
+    Link.add_to_class(field_name, models.URLField(blank=True))
