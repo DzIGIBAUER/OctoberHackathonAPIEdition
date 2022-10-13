598249c099af5f4ad5234656b506b7b3e1761191
DzIGIBAUER_PC <milosh2255@gmail.com>
2022-10-13 21:34:35 +0200

    Search filter can now be applied.

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework_json_api.views import ModelViewSet, RelationshipView
+from rest_framework.filters import SearchFilter
 
 from api.models import Company, Advocate
 from api.serializers import CompanySerializer, AdvocateSerializer
@@ -7,11 +8,13 @@ from api.serializers import CompanySerializer, AdvocateSerializer
 class CompanyViewSet(ModelViewSet):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
-    
+    search_fields = ('name', 'summary',)
+
 
 class AdvocateViewSet(ModelViewSet):
     queryset = Advocate.objects.all()
     serializer_class = AdvocateSerializer
+    search_fields = ('name', 'short_bio', 'long_bio',)
 
 
 class CompanyRelationshipView(RelationshipView):
